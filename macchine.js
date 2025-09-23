f015dcfee8845bad980cd317839bb96917ff4fd2
97guido <39537574+97guido@users.noreply.github.com>
2025-09-23 12:58:58 +0200

    Add files via upload

---
+++ macchine.js
@@ -0,0 +1,540 @@
+var macchine = [
+
+    // Stampa
+    {
+        "reparto": "stampa",
+        "nome": "S100",
+        "costoBaseAsta": 30,
+        "produzione": 150,
+        "costoInGame": 315,	
+        "puntiVita": 10, 	
+        "operatori": 4
+    },
+    {
+        "reparto": "stampa",
+        "nome": "S100 (Upgrade 1)",
+        "costoBaseAsta": 0,
+        "produzione": 60,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "stampa",
+        "nome": "S100 (Upgrade 2)",
+        "costoBaseAsta": 0,
+        "produzione": 75,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "stampa",
+        "nome": "S90",
+        "costoBaseAsta": 20,
+        "produzione": 100,
+        "costoInGame": 210,	
+        "puntiVita": 7, 	
+        "operatori": 3
+    },
+    {
+        "reparto": "stampa",
+        "nome": "S90 (Upgrade 1)",
+        "costoBaseAsta": 0,
+        "produzione": 40,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "stampa",
+        "nome": "S90 (Upgrade 2)",
+        "costoBaseAsta": 0,
+        "produzione": 50,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "stampa",
+        "nome": "DHD",
+        "costoBaseAsta": 10,
+        "produzione": 40,
+        "costoInGame": 145,	
+        "puntiVita": 3, 	
+        "operatori": 3
+    },
+    {
+        "reparto": "stampa",
+        "nome": "DMD",
+        "costoBaseAsta": 10,
+        "produzione": 50,
+        "costoInGame": 105,	
+        "puntiVita": 4, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "stampa",
+        "nome": "DMD (Upgrade 1)",
+        "costoBaseAsta": 0,
+        "produzione": 30,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "stampa",
+        "nome": "DMD (Upgrade 2)",
+        "costoBaseAsta": 0,
+        "produzione": 35,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+
+    // Verniciatura // S80, S60, S110
+    
+    {
+        "reparto": "verniciatura",
+        "nome": "S80",
+        "costoBaseAsta": 20,
+        "produzione": 100,
+        "costoInGame": 210,	
+        "puntiVita": 7, 	
+        "operatori": 2
+    },
+    {
+        "reparto": "verniciatura",
+        "nome": "S60",
+        "costoBaseAsta": 20,
+        "produzione": 100,
+        "costoInGame": 210,	
+        "puntiVita": 7, 	
+        "operatori": 2
+    },
+    {
+        "reparto": "verniciatura",
+        "nome": "S110",
+        "costoBaseAsta": 15,
+        "produzione": 75,
+        "costoInGame": 158,	
+        "puntiVita": 5, 	
+        "operatori": 2
+    },
+
+    // Presse
+    {
+        "reparto": "presse",
+        "nome": "P1",
+        "costoBaseAsta": 1,
+        "produzione": 5,
+        "costoInGame": 11,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P1 (Upgrade 1)",
+        "costoBaseAsta": 0,
+        "produzione": 6,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "presse",
+        "nome": "P1 (Upgrade 2)",
+        "costoBaseAsta": 0,
+        "produzione": 9,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "presse",
+        "nome": "P15",
+        "costoBaseAsta": 1,
+        "produzione": 5,
+        "costoInGame": 11,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P15 (Upgrade 1)",
+        "costoBaseAsta": 0,
+        "produzione": 6,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "presse",
+        "nome": "P15 (Upgrade 2)",
+        "costoBaseAsta": 0,
+        "produzione": 9,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "presse",
+        "nome": "P11",
+        "costoBaseAsta": 1,
+        "produzione": 5,
+        "costoInGame": 11,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P11 (Upgrade 1)",
+        "costoBaseAsta": 0,
+        "produzione": 6,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "presse",
+        "nome": "P11 (Upgrade 2)",
+        "costoBaseAsta": 0,
+        "produzione": 9,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "presse",
+        "nome": "P13",
+        "costoBaseAsta": 1,
+        "produzione": 5,
+        "costoInGame": 11,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P13 (Upgrade 1)",
+        "costoBaseAsta": 0,
+        "produzione": 6,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "presse",
+        "nome": "P13 (Upgrade 2)",
+        "costoBaseAsta": 0,
+        "produzione": 9,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "presse",
+        "nome": "P7",
+        "costoBaseAsta": 1,
+        "produzione": 5,
+        "costoInGame": 11,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P7 (Upgrade 1)",
+        "costoBaseAsta": 0,
+        "produzione": 6,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "presse",
+        "nome": "P7 (Upgrade 2)",
+        "costoBaseAsta": 0,
+        "produzione": 9,
+        "costoInGame": 0,	
+        "puntiVita": 0, 	
+        "operatori": 0
+    },
+    {
+        "reparto": "presse",
+        "nome": "P8",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P9",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P14",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P2",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P3",
+        "costoBaseAsta": 3,
+        "produzione": 15,
+        "costoInGame": 32,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P4",
+        "costoBaseAsta": 3,
+        "produzione": 15,
+        "costoInGame": 32,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P5",
+        "costoBaseAsta": 3,
+        "produzione": 15,
+        "costoInGame": 32,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P12",
+        "costoBaseAsta": 3,
+        "produzione": 15,
+        "costoInGame": 32,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P16",
+        "costoBaseAsta": 3,
+        "produzione": 15,
+        "costoInGame": 32,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P18",
+        "costoBaseAsta": 3,
+        "produzione": 15,
+        "costoInGame": 32,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P19",
+        "costoBaseAsta": 3,
+        "produzione": 15,
+        "costoInGame": 32,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P20",
+        "costoBaseAsta": 4,
+        "produzione": 20,
+        "costoInGame": 42,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P21",
+        "costoBaseAsta": 4,
+        "produzione": 20,
+        "costoInGame": 42,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "presse",
+        "nome": "P22",
+        "costoBaseAsta": 4,
+        "produzione": 20,
+        "costoInGame": 42,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+
+
+
+    // Cesoie
+    {
+        "reparto": "cesoie",
+        "nome": "C1",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "C2",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "C3",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "C4",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "C5",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "C6",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "C7",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "C8",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "C9",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "C10",
+        "costoBaseAsta": 2,
+        "produzione": 10,
+        "costoInGame": 21,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "C30",
+        "costoBaseAsta": 3,
+        "produzione": 15,
+        "costoInGame": 32,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "C31",
+        "costoBaseAsta": 3,
+        "produzione": 15,
+        "costoInGame": 32,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "C32",
+        "costoBaseAsta": 3,
+        "produzione": 15,
+        "costoInGame": 32,	
+        "puntiVita": 2, 	
+        "operatori": 1
+    },
+    {
+        "reparto": "cesoie",
+        "nome": "SUPERCESOIA",
+        "costoBaseAsta": null,
+        "produzione": 60,
+        "costoInGame": null,	
+        "puntiVita": 5, 	
+        "operatori": 1
+    },
+
+    // Taglio T1, T2
+    {
+        "reparto": "centroTaglio",
+        "nome": "T1",
+        "costoBaseAsta": 20,
+        "produzione": 60,
+        "costoInGame": 130,	
+        "puntiVita": 6, 	
+        "operatori": 2
+    },
+    {
+        "reparto": "centroTaglio",
+        "nome": "T2",
+        "costoBaseAsta": 20,
+        "produzione": 60,
+        "costoInGame": 130,	
+        "puntiVita": 6, 	
+        "operatori": 2
+    },
+    
+    
+
+]
