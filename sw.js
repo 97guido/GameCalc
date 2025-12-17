b3974cf0184a0edccff6a3d4364a3f0d6fb6fe71
Simo (Chuwi) <97guido@gmail.com>
2025-12-17 12:39:25 +0100

    Test fullscreen Add icon

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Nome della cache (cambialo quando aggiorni i file del sito)
+const CACHE_NAME = 'gamecalc-pwa-v1';
+// Lista dei file da salvare per l'utilizzo offline
+const urlsToCache = [
+  './',
+  './index.html',
+  './index.css',
+  './main.js',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+// Installazione: salva i file nella cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Intercettazione richieste: serve i file dalla cache se offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
